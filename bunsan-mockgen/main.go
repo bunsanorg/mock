5bb8ab67cdaeffc3a1c1e800f7adde0c35562ec7
Aleksey Filippov <sarum9in@gmail.com>
2015-10-20 20:50:32 +0400

    Fixed Go style.

--- bunsan-mockgen/main.go
+++ bunsan-mockgen/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-    "flag"
-    "log"
-    "os"
-    "os/exec"
+	"flag"
+	"log"
+	"os"
+	"os/exec"
 
-    "github.com/bunsanorg/buildutils"
+	"github.com/bunsanorg/buildutils"
 )
 
 var mockgen = flag.String("mockgen", "mockgen", "github.com/golang/mock/mockgen location")
@@ -15,33 +15,33 @@ var destination = flag.String("destination", "", "Destination file")
 var importSelf = flag.Bool("import-self", false, "Import source package")
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    if *source == "" {
-        log.Fatal("-source is required")
-    }
-    if *destination == "" {
-        log.Fatal("-destination is required")
-    }
+	if *source == "" {
+		log.Fatal("-source is required")
+	}
+	if *destination == "" {
+		log.Fatal("-destination is required")
+	}
 
-    cwd, err := os.Getwd()
-    if err != nil {
-        log.Fatal(err)
-    }
-    importPath, err := buildutils.ImportPath(cwd)
-    if err != nil {
-        log.Fatal(err)
-    }
-    arguments := []string{
-        "-source", *source,
-        "-destination", *destination,
-    }
-    if *importSelf {
-        arguments = append(arguments, "-imports", ".="+importPath)
-    }
-    cmd := exec.Command(*mockgen, arguments...)
-    err = cmd.Run()
-    if err != nil {
-        log.Fatal(err)
-    }
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	importPath, err := buildutils.ImportPath(cwd)
+	if err != nil {
+		log.Fatal(err)
+	}
+	arguments := []string{
+		"-source", *source,
+		"-destination", *destination,
+	}
+	if *importSelf {
+		arguments = append(arguments, "-imports", ".="+importPath)
+	}
+	cmd := exec.Command(*mockgen, arguments...)
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
